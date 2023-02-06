02654d235475179c3fd2a400bc8ea9d7af4b0617
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-02-05 20:20:40 -0500

    Cleaning up Vite boilerplate

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+        <App />
+    </>
+)

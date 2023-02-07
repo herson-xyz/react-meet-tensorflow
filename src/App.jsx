e9ef1ad03f92e6bb64ce10fd6907ab73950bbf4a
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-02-06 21:08:19 -0500

    Installing and importing dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// Install dependencies
-// Import dependencies
+// Install dependencies       - DONE
+// Import dependencies        - DONE
 // Setup webcam and canvas
 // Define references to those
 // Load facemesh
@@ -9,6 +9,10 @@
 // Setup triangle path
 // Setup point drawing
 // Add drawMesh to detech function
+
+import * as tf from "@tensorflow/tfjs"
+import * as facemesh from "@tensorflow-models/facemesh"
+import Webcam from "react-webcam"
 
 export default function App() {
 

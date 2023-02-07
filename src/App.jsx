55559cf6e1f0da8d1af864281109c5b69fa070c4
Herson Guerrero Huh <watsrunner+github+herson@gmail.com>
2023-02-06 21:01:05 -0500

    Starting over - Cleaning up files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import WebcamComponent from './WebcamComponent.jsx'
+// Install dependencies
+// Import dependencies
+// Setup webcam and canvas
+// Define references to those
+// Load facemesh
+// Detect function
+// Drawing utilities
+// Load triangulation
+// Setup triangle path
+// Setup point drawing
+// Add drawMesh to detech function
 
 export default function App() {
 
+      
   return <>
-    <WebcamComponent />
-  </>
+
+    </>
 }
